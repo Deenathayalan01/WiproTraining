9e22e1341419200b9880c3023748664055f10f10
Deenathayalan01 <deenapugal2018@gmail.com>
2025-03-18 15:35:06 +0530

    my commit

--- Phase1/StudentManagementApp/StudentMngtScript.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("Script loaded successfully!");
-
-    // Smooth scroll for the timetable link
-    document.querySelector(".nav-link[href='#timetable']").addEventListener("click", function (e) {
-        e.preventDefault();
-        document.querySelector(".timetable").scrollIntoView({ behavior: "smooth" });
-    });
-});
